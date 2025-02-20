35587a03fea8fa61ff3e38803553ec2b5ebcc2b4
Quentin FABLE <quentin.fable@ganil.fr>
2025-02-20 15:22:00 +0100

    Chapter 2: Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,6 +5,7 @@
 # -> View functions are mapped to one (or more) URLs
 # -> i.e Flask knows what to execute when the client request a given URL  
 
+from flask import render_template
 from app import app
 
 # We use decorators to create an association between the URL given as argument and the function
@@ -13,4 +14,23 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	# 'mock' object: we don't have user yet
+	user = {'username': 'Bob'}
+
+	# list of posts for my blog
+	posts = [
+		{
+			'author': {'username': 'Jackie Chan'},
+			'body': 'Beautiful day in Caen!'
+		},
+		{
+			'author': {'username': 'Bobby Brown'},
+			'body': 'I like trains!'
+		},		
+	]
+
+	# The operation that converts a template into a HTML page is called 'rendering'
+	# It is done in Flask using the 'render_template(<filename.html>, <arg>)' function
+	# render_templates invokes Jinja template engine (bundled with Flask)
+	# Jinja substitutes {{ ... }} block with corresponding values provided in the <arg> of the function 
+	return render_template('index.html', title='Home', user=user, posts=posts)
